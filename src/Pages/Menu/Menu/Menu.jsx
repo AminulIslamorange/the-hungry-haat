77c971c3a7e2688a03b90dd41101bc45dc079994
AminulIslamorange <aminulislamorange@gmail.com>
2025-04-20 18:53:56 +0600

    menu page started

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet-async";
+
+
+const Menu = () => {
+    return (
+        <div>
+              <Helmet>
+        <title>Hungry | menu</title>
+        
+      </Helmet>
+        </div>
+    );
+};
+
+export default Menu;
