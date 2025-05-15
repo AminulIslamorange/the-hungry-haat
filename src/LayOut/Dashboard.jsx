a5a0dfc416a9421fce9412abaa467e2d6ab778d2
AminulIslamorange <aminulislamorange@gmail.com>
2025-05-15 20:17:45 +0600

    cart calculation added

--- src/LayOut/Dashboard.jsx
+++ src/LayOut/Dashboard.jsx
@@ -1,5 +1,9 @@
 
 import { FaCalendar, FaHome, FaPaypal, FaShoppingCart, } from "react-icons/fa";
+
+import { IoMenu } from "react-icons/io5";
+import { FaShopify } from "react-icons/fa";
+import { MdOutlineContactPhone } from "react-icons/md";
 import { TbBrandBooking } from "react-icons/tb";
 import { GoCodeReview } from "react-icons/go";
 import { NavLink, Outlet } from "react-router-dom";
@@ -29,10 +33,25 @@ const Dashboard = () => {
                             <li>
                                 <NavLink to='/dashboard/booking'><TbBrandBooking />My Booking</NavLink>
                             </li>
+                             <div className="divider"></div>
+                    {/* common navlink for admin and user */}
+                    <li>
+                        <NavLink to='/'><FaHome />  Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/menu'> <IoMenu />
+                            Menu</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/order/salad'><FaShopify />  Shop</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/'><MdOutlineContactPhone /> Contact</NavLink>
+                    </li>
                 </ul>
             </div>
             {/* for dashboard related table or data dashboad content */}
-            <div>
+            <div className="flex-1 m-4 p-2">
                 <Outlet></Outlet>
             </div>
         </div>
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,76 @@
+import { Link } from "react-router-dom";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useCart from "../../../hooks/useCart";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 
 const Cart = () => {
+    const [cart]=useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
     return (
-        <div>
-            <h2 className="text-5xl">My Cart</h2>
+         <div>
+            <div>
+                <SectionTitle heading={'WANNA ADD MORE?'} subHeading={'---My Cart---'}></SectionTitle>
+            </div>
+            <div className="bg-[#FFF]">
+                <div className="flex text-3xl justify-evenly mt-4">
+                    <h2 className="ml-2">Total orders: {cart.length}</h2>
+                    <p>total price: ${totalPrice}</p>
+                  { cart.length ?
+                     <Link to='/dashboard/payment'> <button className="bg-[#D1A054] px-3">Pay</button></Link>
+                  : <button className="bg-[#D1A054] px-3">Pay</button>}
+                </div>
+                <div className="overflow-x-auto mt-8">
+                    <table className="table ">
+                        {/* head */}
+                        <thead className=" mx-2">
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>ITEM IMAGE</th>
+                                <th>ITEM NAME</th>
+                                <th>PRICE</th>
+                                <th>ACTION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart.map((item, index) => <tr key={item._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={item.image}
+                                                    alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td>$:{item.price}</td>
+                                <th>
+                                    <button 
+                                        className="btn btn-ghost text-xl bg-[#B91C1C]"><RiDeleteBin6Line /></button>
+                                </th>
+                            </tr>)}
+
+
+
+
+
+                        </tbody>
+
+
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
